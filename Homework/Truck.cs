917f48aa699ba8a38677ce065217518fc65c631b
igor <cobrda136@gmail.com>
2024-12-15 20:47:59 +0100

    Domáca úloha Lekcia 8 Igor Čobrda

---
+++ Homework/Truck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Homework
+{
+    internal class Truck : Vehicle, IDriveable
+    {
+        private int _tonaz;     // tonaz vozidla
+        public Truck(int idVozidla, string popisVozidla, int tonaz) : base(idVozidla, popisVozidla)
+        {
+            _tonaz = tonaz;
+        }
+        public int GetTonaz()
+        {
+            return _tonaz;
+        }
+        public void SetModel(int tonaz)
+        {
+            _tonaz = tonaz;
+        }
+
+        public int Drive()
+        {
+            return 10;
+        }
+
+        public override void VodicskeOpravnenie()
+        {
+            Console.WriteLine("Vodič nákladného auta");
+        }
+
+        public override void UcelJazdy()
+        {
+            Console.WriteLine("Preprava tovaru");
+        }
+    }
+}
